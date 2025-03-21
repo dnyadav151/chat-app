982cc4b610b6ca34b64307a3b58d9e3aeda7b486
Dhruv Narayan Yadav <111013065+dnyadav151@users.noreply.github.com>
2025-03-21 14:07:50 +0530

    added / in chat.js

--- client/src/apiCalls/chat.js
+++ client/src/apiCalls/chat.js
@@ -3,7 +3,7 @@ import {axiosInstance, url } from './index';
 
 export const getAllChats = async () => {
     try {
-        const response = await axiosInstance.get(url + 'api/chat/get-all-chats');
+        const response = await axiosInstance.get(url + '/api/chat/get-all-chats');
         return response.data;
     } catch (error) {
         return error;
@@ -13,7 +13,7 @@ export const getAllChats = async () => {
 
 export const createNewChat = async ( members ) => {
     try {
-        const response = await axiosInstance.post(url + 'api/chat/create-new-chat', { members });
+        const response = await axiosInstance.post(url + '/api/chat/create-new-chat', { members });
         return response.data;
     } catch (error) {
         return error;
@@ -23,7 +23,7 @@ export const createNewChat = async ( members ) => {
 
 export const clearUnreadMessageCount = async ( chatId ) => {
     try {
-        const response = await axiosInstance.post('api/chat/clear-unread-message', { chatId: chatId });
+        const response = await axiosInstance.post('/api/chat/clear-unread-message', { chatId: chatId });
         return response.data;
     } catch (error) {
         return error;
